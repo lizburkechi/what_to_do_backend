ca0c251168231cfce167ce16ccbf5dc640b4848c
lizburkechi <liz.burke.chi@gmail.com>
2021-08-03 14:05:26 -0500

    more activities added

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,86 +49,197 @@ activities = Activity.create([
         link: "https://www.spotify.com/us/"
     },
     {
-        activity_name: "Learn Express.js",
+        activity_name: "Go to the Field Museum",
         category: "education",
         participants: 1,
-        link: "https://expressjs.com/"
+        link: "https://www.fieldmuseum.org/visit/accessibility"
     },
     {
-        activity_name: "Meditate for 5 Minutes",
-        category: "relaxation",
+        activity_name: "Visit Navy Pier",
+        category: "recreation",
         participants: 1,
-        link: "https://www.headspace.com/meditation/5-minute-meditation"
+        link: "https://navypier.org/plan-your-visit/guest-services-and-accessibility/"
     },
     {
-        activity_name: "See an Event at the United Center",
-        category: "recreational",
-        participants: 1,
-        link: "https://www.unitedcenter.com/"
-    },
-    {
-        activity_name: "See a Movie with a Few Friends at Regal Webster Place",
-        category: "social",
-        participants: 4,
-        link: "https://www.regmovies.com/theatres/regal-webster-place/1793#/"
-    },
-    {
-        activity_name: "Check out a New Music Genre with a Friend",
+        activity_name: "Go to a Show at Subterranean",
         category: "music",
+        participants: 1,
+        link: "https://www.subt.net/faqs/"
+    },
+    {
+        activity_name: "Go to a White Sox Game",
+        category: "sports",
+        participants: 4,
+        link: "https://www.mlb.com/whitesox/ballpark/disability-access-guide"
+    },
+    {
+        activity_name: "Visit Garfield Park Conservatory",
+        category: "education",
         participants: 2,
-        link: "https://www.spotify.com/us/"
+        link: "https://garfieldconservatory.org/accessibility/"
+    },
+    {
+        activity_name: "Take the Chicago Architecture Boat Tour",
+        category: "recreation",
+        participants: 1,
+        link: "https://www.cruisechicago.com/accessibility"
+    },
+    {
+        activity_name: "Visit the DuSable Museum of African American History",
+        category: "education",
+        participants: 1,
+        link: "https://easyaccesschicago.org/access_info/museums.php?id=8"
+    },
+    {
+        activity_name: "Get Dinner with Friends at Ja'Grill",
+        category: "restaurants",
+        participants: 4,
+        link: "https://www.jagrill.com/"
+    },
+    {
+        activity_name: "Have Dinner and Drinks at Alinea",
+        category: "restaurants",
+        participants: 2,
+        link: "https://www.alinearestaurant.com/"
+    },
+    {
+        activity_name: "Grab a Drink at Little Bad Wolf",
+        category: "bars",
+        participants: 1,
+        link: "https://www.littlebadwolfchicago.com/"
+    },
+    {
+        activity_name: "Walk the Boardwalk at Lincoln Park Zoo",
+        category: "outdoors",
+        participants: 1,
+        link: "https://www.lpzoo.org/visit/accessibility/"
+    },
+    {
+        activity_name: "See a Bulls Game at the United Center",
+        category: "sports",
+        participants: 1,
+        link: "https://www.unitedcenter.com/venue/accessibility-guide/"
+    },
+    {
+        activity_name: "Go up to the Skydeck at Willis Tower (Sears Tower)",
+        category: "recreation",
+        participants: 1,
+        link: "https://wheelchairtravel.org/chicago/attractions/"
+    },
+    {
+        activity_name: "See a Show at the Chicago Theatre",
+        category: "entertainment",
+        participants: 1,
+        link: "https://www.msg.com/the-chicago-theatre/disabled-services"
+    },
+    {
+        activity_name: "Eat some Chicago Style Pizza with a Friend at Pizzeria Uno",
+        category: "restaurants",
+        participants: 2,
+        link: "https://www.pizzeriaunodue.com/"
     },
 ])
+
 
 notes = Note.create!([
     {
         content: "Expressjs.com is compatible with google screen reader extension and other screen reading technologies for visually impaired individuals.",
         user_id: User.first.id,
-        activity_id: Activity.first.id
+        activity_id: 1,
     },
     {
         content: "Headspace.com offers a listen along audio track for visually impaired persons, however is not suitable for people with auditory impairment. An ASL meditation playlist is available at https://www.youtube.com/playlist?list=PL4WhbP0xwGB7syvK57c3sSW0B9gKQs7Px ",
         user_id: User.second.id,
-        activity_id: Activity.second.id
+        activity_id: 2,
     },
     {
         content: "All ATMs are marked in braille and can be found at the following locations: Box Office lobby near Gate 4, 100 level concourse at section 105/106 and 117/118, 200 level at section 220/221 and on the 300 level concourse at section 327/328. Each of the 46 concession stands are wheelchair accessible and elevators are located next to Gates 3 and 7 for you and a companion.",
         user_id: User.third.id,
-        activity_id: Activity.third.id
+        activity_id: 3,
     },
     {
         content: "Fully accessible bathrooms and parking garage with elevator to theatre level. Open spaces within the theates for wheelchairs but no handicap accessible / disabled aware seating outside the theater. Only tall chairs at tall tables.",
-        user_id: User.first.id,
-        activity_id: Activity.fourth.id
+        user_id: User.fourth.id,
+        activity_id: 4,
     },
     {
         content: "Spotify only offers lyrics to select songs and is not fully accessible to the hearing impared. They have recently updated the app so that mobile devices set to 'Larger Text' will apply changes within the app to make text easier to read for the visdually impaired.",
         user_id: User.second.id,
-        activity_id: Activity.fifth.id
+        activity_id: 5,
     },{
-        content: "Expressjs.com is compatible with google screen reader extension and other screen reading technologies for visually impaired individuals.",
+        content: "Highly reccommend Field Museum for All, an app for planning your visit with accessibility in mind. It offers a customizable scheduling tool, a sensory friendly map of the museum and exibition previews to better inform guests with sensory disabilities what to expect.",
         user_id: User.first.id,
-        activity_id: Activity.fourth.id
+        activity_id: 6,
     },
     {
-        content: "Headspace.com offers a listen along audio track for visually impaired persons, however is not suitable for people with auditory impairment. An ASL meditation playlist is available at https://www.youtube.com/playlist?list=PL4WhbP0xwGB7syvK57c3sSW0B9gKQs7Px ",
-        user_id: User.second.id,
-        activity_id: Activity.second.id
+        content: "There are no maps available with ADA accessible locations marked despite advertising it on thier website.",
+        user_id: User.fourth.id,
+        activity_id: 7,
     },
     {
-        content: "All ATMs are marked in braille and can be found at the following locations: Box Office lobby near Gate 4, 100 level concourse at section 105/106 and 117/118, 200 level at section 220/221 and on the 300 level concourse at section 327/328. Each of the 46 concession stands are wheelchair accessible and elevators are located next to Gates 3 and 7 for you and a companion.",
+        content: "The website says that the first floor is wheelchair accessible including bathrooms and is not true. There is an eight inch (platform) stage which you need to be able to pass over to reach the 'accessible' bathroom.",
         user_id: User.third.id,
-        activity_id: Activity.fourth.id
+        activity_id: 8,
     },
     {
-        content: "Fully accessible bathrooms and parking garage with elevator to theatre level. Open spaces within the theates for wheelchairs but no handicap accessible / disabled aware seating outside the theater. Only tall chairs at tall tables.",
+        content: "We were happily surprised to find out that you can request a dedicated ASL interpreter on the website in advance. Once we got there they also let us know that often even without notice there are interpreters on hand available!",
         user_id: User.fifth.id,
-        activity_id: Activity.fourth.id
+        activity_id: 9,
     },
     {
-        content: "Spotify only offers lyrics to select songs and is not fully accessible to the hearing impared. They have recently updated the app so that mobile devices set to 'Larger Text' will apply changes within the app to make text easier to read for the visdually impaired.",
+        content: "Call ahead if you will need to use a wheelchair. They are available but limited!",
         user_id: User.fifth.id,
-        activity_id: Activity.fifth.id
+        activity_id: 10,
+    },
+    {
+        content: "They will not allow you to bring your motorized wheelchair or scooter on board but offer manual wheelchairs for people who can transfer on site.",
+        user_id: User.fourth.id,
+        activity_id: 11,
+    },
+    {
+        content: "Some exhibit videos are captioned.",
+        user_id: User.second.id,
+        activity_id: 12,
+    },
+    {
+        content: "The inside gets very packed but there is some outdoor seating if you are looking for a more sensory friendly experience!",
+        user_id: User.third.id,
+        activity_id: 13,
+    },
+    {
+        content: "First floor only offers wheelchair accessible dining and bathrooms.",
+        user_id: User.fifth.id,
+        activity_id: 14,
+    },
+    {
+        content: "Offers wheelchair accessible seating and bathroooms.",
+        user_id: User.fifth.id,
+        activity_id: 15,
+    },
+    {
+        content: "Great outdoor experience for those with sensory disabilities.",
+        user_id: User.first.id,
+        activity_id: 16,
+    },
+    {
+        content: "You can pick up personal headsets (assistive listening) for free at the guest relations booth!",
+        user_id: User.second.id,
+        activity_id: 17,
+    },
+    {
+        content: "Elevator access is availble to the skydeck.",
+        user_id: User.third.id,
+        activity_id: 18,
+    },
+    {
+        content: "Heads up - Personal listening devices are available at the coat check!",
+        user_id: User.fourth.id,
+        activity_id: 19,
+    },
+    {
+        content: "Very loud not reccommended for those with sensory or auditory related disabilities. Offers some outdoor seating depending on weather.",
+        user_id: User.fifth.id,
+        activity_id: 20,
     },
 ])
 
